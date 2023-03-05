33f9e75ff31aca8bc0e61cd6040636117f4068f0
brenna11 <103007668+brenna11@users.noreply.github.com>
2023-03-05 18:03:06 -0500

    completed version of postagram

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,12 @@
 import { appName } from "../../includes/variable";
-import { RiMickeyLine } from "react-icons/ri";
+import { CgImage } from "react-icons/cg";
 import './styles.scss';
 
 export default function Header() {
     return (
         <header className="main">
+             <CgImage />
             <h1>{appName}</h1>
-            <RiMickeyLine />
         </header>
     );
 }
